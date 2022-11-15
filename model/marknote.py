9d9ef74bec80d4a463772c2c88f672b48e00a43f
Avill1 <anastaiya6379753@gmail.com>
2022-11-15 21:46:48 +0300

    new

---
+++ model/marknote.py
@@ -0,0 +1,25 @@
+class MarkNote:
+    def __init__(self):
+        self._marks = []
+
+
+    def __add__(self, mark):
+        self._marks.append(mark)
+
+    def get(self, index):
+        return self._marks[index]
+
+    def size(self):
+        retyrn len(self._marks)
+
+
+marknote = MarkNote()
+
+marknote.add(8)
+marknote.add(9)
+marknote.add(10)
+marknote.add(5)
+marknote.add(6)
+
+
+for  in marknote
